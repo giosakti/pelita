3c5402d52eedcc497af02618fee904df7e9f9c40
Giovanni Sakti <giosakti@starqle.com>
2017-06-29 19:44:15 +0700

    rearrange namespace

--- lib/pelita/operation/attempt_adapter.rb
+++ lib/pelita/operation/attempt_adapter.rb
@@ -1,16 +1,20 @@
-class AttemptAdapter < Dry::Transaction::StepAdapters
-  include Dry::Monads::Either::Mixin
+module Pelita
+  module Operation
+    class AttemptAdapter < Dry::Transaction::StepAdapters
+      include Dry::Monads::Either::Mixin
 
-  def call(step, input, *args)
-    unless step.options[:catch]
-      raise ArgumentError, "+attempt+ steps require one or more exception classes provided via +catch:+"
+      def call(step, input, *args)
+        unless step.options[:catch]
+          raise ArgumentError, "+attempt+ steps require one or more exception classes provided via +catch:+"
+        end
+
+        step.operation.call(input, *args)
+      rescue *Array(step.options[:catch]) => e
+        e = step.options[:raise].new(e.message) if step.options[:raise]
+        input["error"] = e
+        Left(input)
+      end
     end
-
-    step.operation.call(input, *args)
-  rescue *Array(step.options[:catch]) => e
-    e = step.options[:raise].new(e.message) if step.options[:raise]
-    input["error"] = e
-    Left(input)
   end
 end
-Dry::Transaction::StepAdapters.register :attempt, AttemptAdapter.new
+Dry::Transaction::StepAdapters.register :attempt, Pelita::Operation::AttemptAdapter.new
--- lib/pelita/operation/base.rb
+++ lib/pelita/operation/base.rb
@@ -1,16 +1,18 @@
 require_relative 'attempt_adapter'
 
 module Pelita
-  class Operation
-    include Dry::Transaction
+  module Operation
+    class Base
+      include Dry::Transaction
 
-    def authorize!(options)
-      if options["current_user"].signed_in?
-        options["result.policy.default"] = true
-        Right(options)
-      else
-        options["result.policy.default"] = false
-        Left(options)
+      def authorize!(options)
+        if options["current_user"].signed_in?
+          options["result.policy.default"] = true
+          Right(options)
+        else
+          options["result.policy.default"] = false
+          Left(options)
+        end
       end
     end
   end
