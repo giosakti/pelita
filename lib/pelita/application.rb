0497adaf05b5a77696c1fb39f959ee11956e00ee
Giovanni Sakti <giosakti@starqle.com>
2017-06-30 07:50:31 +0700

    add method for generating connection string

--- lib/pelita/application.rb
+++ lib/pelita/application.rb
@@ -11,5 +11,25 @@ module Pelita
     route do |r|
       r.multi_run
     end
+
+    def self.generate_connection_string(db_config)
+      conn_string = db_config['adapter']
+
+      unless db_config['host'].blank?
+        host_string = db_config['host']
+        host_string = "#{host_string}:#{db_config['port']}" unless db_config['port'].blank?
+
+        unless db_config['username'].blank?
+          user_string = db_config['username']
+          user_string = "#{user_string}:#{db_config['password']}" unless db_config['password'].blank?
+          host_string = "#{user_string}@#{host_string}"
+        end
+
+        conn_string = "#{conn_string}://#{host_string}"
+        conn_string = "#{conn_string}/#{db_config['database']}" unless db_config['database'].blank?
+      end
+
+      return conn_string
+    end
   end
 end
