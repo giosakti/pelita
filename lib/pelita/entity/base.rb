81ba6f8ebd976a6db4902b0a0ca418e23f2beb8b
Giovanni Sakti <giosakti@gmail.com>
2017-06-30 22:57:21 +0700

    create entity & repository base classes

---
+++ lib/pelita/entity/base.rb
@@ -0,0 +1,7 @@
+module Pelita
+  module Entity
+    class Base < Dry::Struct
+      constructor_type :schema
+    end
+  end
+end
--- lib/pelita/pelita.rb
+++ lib/pelita/pelita.rb
@@ -1,3 +1,5 @@
 require_relative 'container'
 require_relative 'application'
+require_relative 'entity/base'
+require_relative 'repository/base'
 require_relative 'operation/base'
