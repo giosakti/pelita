38f6ab7c017b9ea9a4384d7c0c1afc3ce8e74de6
opanmustopah <opan.neutron@gmail.com>
2017-07-04 13:51:36 +0700

    Add rubocop and add class method to get db configuration

--- lib/pelita/application/base.rb
+++ lib/pelita/application/base.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'erb'
+
 module Pelita
   module Application
     class Base < Roda
@@ -9,28 +13,36 @@ module Pelita
 
       # Setup routing tree
       plugin :multi_run
-      route do |r|
-        r.multi_run
-      end
+      route(&:multi_run)
 
       def self.generate_connection_string(db_config)
         conn_string = db_config['adapter']
 
         unless db_config['host'].blank?
           host_string = db_config['host']
-          host_string = "#{host_string}:#{db_config['port']}" unless db_config['port'].blank?
+          host_string = "#{host_string}:#{db_config["port"]}" unless db_config['port'].blank?
 
           unless db_config['username'].blank?
             user_string = db_config['username']
-            user_string = "#{user_string}:#{db_config['password']}" unless db_config['password'].blank?
+            user_string = "#{user_string}:#{db_config["password"]}" unless db_config['password'].blank?
             host_string = "#{user_string}@#{host_string}"
           end
 
           conn_string = "#{conn_string}://#{host_string}"
-          conn_string = "#{conn_string}/#{db_config['database']}" unless db_config['database'].blank?
+          conn_string = "#{conn_string}/#{db_config["database"]}" unless db_config['database'].blank?
         end
 
-        return conn_string
+        conn_string
+      end
+
+      def self.db_config
+        # Load db configuration
+        db_config = File.read("#{config.root}/config/database.yml")
+        db_config = ERB.new(db_config).result
+        db_config = YAML.safe_load(db_config)
+
+        # Setup connection string
+        db_config[config.env]
       end
     end
   end
--- pelita.gemspec
+++ pelita.gemspec
@@ -39,4 +39,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "database_cleaner", "~> 1.6"
   spec.add_development_dependency "rspec", "~> 3.6"
+  spec.add_development_dependency "rubocop"
 end
