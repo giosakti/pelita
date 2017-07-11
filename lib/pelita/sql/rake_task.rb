3faa43a48e107cd3920397d726b5231a3bedd562
Giovanni Sakti <giosakti@gmail.com>
2017-07-11 07:26:01 +0700

    tasks for creating and dropping database

--- lib/pelita/sql/rake_task.rb
+++ lib/pelita/sql/rake_task.rb
@@ -1,1 +1,57 @@
 require 'rom/sql/rake_task'
+require 'pelita'
+require 'sequel'
+
+namespace :db do
+  # Setup connection string
+  pelita = ::Pelita::Application::Base
+  db_config = pelita.
+    fetch_db_config("#{pelita.config.root}/config/database.yml")[pelita.config.env]
+  conn_string = pelita.generate_connection_string(db_config)
+
+  # TODO: @giosakti make this database agnostic
+  desc 'Create database'
+  task :create do
+    DB = Sequel.send(
+      db_config['adapter'].to_sym,
+      'postgres',
+      user: db_config['username'],
+      password: db_config['password'],
+      host: db_config['host'],
+      port: db_config['port']
+    )
+
+    begin
+      puts "execute db:create"
+      DB.run("CREATE DATABASE #{db_config["database"]} OWNER #{db_config["username"]}")
+    rescue Sequel::DatabaseError
+      puts "Database #{db_config["database"]} already exists"
+    end
+
+    DB.disconnect
+
+    Rake::Task['db:setup'].invoke
+  end
+
+  # TODO: @giosakti make this database agnostic
+  desc 'Destroy database'
+  task :drop do
+    DB = Sequel.send(
+      db_config['adapter'].to_sym,
+      'postgres',
+      user: db_config['username'],
+      password: db_config['password'],
+      host: db_config['host'],
+      port: db_config['port']
+    )
+
+    begin
+      puts 'execute db:drop'
+      DB.run("DROP DATABASE #{db_config["database"]}")
+    rescue Sequel::DatabaseError
+      puts "Database #{db_config["database"]} didn't exists or already dropped"
+    end
+
+    DB.disconnect
+  end
+end
