eb268f81be662232f8b17710903e93922f5b4511
Giovanni Sakti <giosakti@gmail.com>
2017-07-20 13:43:46 +0700

    current_user must be defined on a per-system basis

--- lib/pelita/auth/helpers.rb
+++ lib/pelita/auth/helpers.rb
@@ -22,16 +22,6 @@ module Pelita
       def logout(scope = nil)
         scope ? warden.logout(scope) : warden.logout(warden.config.default_scope)
       end
-
-      def user(scope = nil)
-        scope ? warden.user(scope) : warden.user
-      end
-      alias_method :current_user, :user
-
-      def user=(new_user, opts = {})
-        warden.set_user(new_user, opts)
-      end
-      alias_method :current_user=, :user=
     end
   end
 end
